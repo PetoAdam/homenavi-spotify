1dcbd5747c3b3714d33d69d53eebe68c4ee39e0e
PetoAdam <peto.adam@yahoo.com>
2026-02-08 12:13:02 +0100

    feat: add overflow styles for hn-tab body class and manage class in TabApp component

--- src/frontend/src/tab/main.jsx
+++ src/frontend/src/tab/main.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import '../shared/hn.css';
 import Player from '../shared/Player';
 
 function TabApp() {
+  useEffect(() => {
+    document.body.classList.add('hn-tab');
+    return () => {
+      document.body.classList.remove('hn-tab');
+    };
+  }, []);
+
   return (
     <div className="hn-shell hn-tab-shell">
       <Player variant="tab" showSearch showQueue />
